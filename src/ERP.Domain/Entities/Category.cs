ee1728b83857255d5478524e7f7675f067b5ea12
katescaner <jjmateo1279@gmail.com>
2026-06-17 14:12:07 +0200

    refactor: reorganize solution folder structure

---
+++ src/ERP.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Domain.Entities;
+
+public class Category : BaseEntity {
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
